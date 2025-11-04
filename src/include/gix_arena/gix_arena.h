4251d32a591d7da7e389ccbfeb71652428c3ca15
Yogi Astawan <yogi.astawan@gmail.com>
2025-11-04 15:01:18 +0800

    add header utils to include

--- src/include/gix_arena/gix_arena.h
+++ src/include/gix_arena/gix_arena.h
@@ -1,3 +1,5 @@
 
 #include <gix_arena/core/gix_arena_impl.h>
 #include <gix_arena/core/gix_arena_version.h>
+#include <gix_arena/utils/gix_arena_define.h>
+#include <gix_arena/utils/gix_arena_platform.h>
